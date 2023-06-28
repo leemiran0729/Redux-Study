9681210fcb17556e0bfffa96f0aa16a940861bb9
leemiran0729 <leemiran.0729@gmail.com>
2023-06-28 14:16:20 +0900

    chore, feat: redux 설치 및 store, reducer 생성

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,23 +1,18 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
-let count = 0;
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+//state를 변화시켜주는 건 reducer 뿐!
+const countModifier = (count = 0) => {
+  /*
+  count를 변화시키는 방법은 여러개 (ex) 증가하거나 감소 등)
+  하나의 리듀서에 action 여러 개를 나눠 state를 변화시켜주자!
+  */
+  //변화시켜준 후 return
+  return count;
 };
 
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+//store 생성: state를 저장하는 곳!
+const countStore = createStore;
