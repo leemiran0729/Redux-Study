d82a74ff16acb95426d31ddf8e4866c7c2a45394
leemiran0729 <leemiran.0729@gmail.com>
2023-06-30 15:45:15 +0900

    feat: paintTodos 함수 구현

--- vanilla-redux-todo/src/index.js
+++ vanilla-redux-todo/src/index.js
@@ -10,7 +10,7 @@ const DELETE_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -20,15 +20,18 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-const createTodo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const paintTodos = () => {
+  ul.innerHTML = "";
+  const toDos = store.getState();
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+  });
 };
+
+store.subscribe(paintTodos);
 
 const addTodo = (text) => {
   store.dispatch({ type: ADD_TODO, text });
